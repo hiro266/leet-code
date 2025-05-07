83d2f8d085a0c402c0b0d4a5a117f62cc1dc9788
hiro266 <52743869+hiro266@users.noreply.github.com>
2025-05-07 21:21:20 +0900

    Time: 775 ms (23.29%) | Memory: 69 MB (15.39%) - LeetSync

---
+++ 189-rotate-array/rotate-array.ts
@@ -0,0 +1,9 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function rotate(nums: number[], k: number): void {
+  for (let i = 0; i < k; i++) {
+    nums.unshift(nums[nums.length - 1]);
+    nums.pop();
+  }
+};
