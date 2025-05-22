368ed5050930d46cb1a76697a23490b0fc0cc850
hiro266 <52743869+hiro266@users.noreply.github.com>
2025-05-23 00:06:34 +0900

    Time: 6 ms (61.90%) | Memory: 62.4 MB (64.57%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.ts
+++ 12-integer-to-roman/integer-to-roman.ts
@@ -1,58 +1,29 @@
-const getRomanNum = (value: number): [string, number] => {
-  // 1000~
-  if (value >= 1000) {
-    return ["M", 1000];
-    // 999 ~ 900
-  } else if (value < 1000 && value > 899) {
-    return ["CM", 900];
-    // 899 ~ 500
-  } else if (value < 900 && value > 499) {
-    return ["D", 500];
-    // 499 ~ 400
-  } else if (value < 500 && value > 399) {
-    return ["CD", 400];
-    // 399 ~ 100
-  } else if (value < 400 && value > 99) {
-    return ["C", 100];
-    // 99 ~ 90
-  } else if (value < 100 && value > 89) {
-    return ["XC", 90];
-    // 89 ~ 50
-  } else if (value < 90 && value > 49) {
-    return ["L", 50];
-    // 49 ~ 40
-  } else if (value < 50 && value > 39) {
-    return ["XL", 40];
-    // 39 ~ 10
-  } else if (value < 40 && value > 9) {
-    return ["X", 10];
-    // 9 ~ 6
-  } else if (value === 9) {
-    return ["IX", 9];
-    // 5 ~ 4
-  } else if (value < 9 && value > 4) {
-    return ["V", 5];
-    //
-  } else if (value === 4) {
-    return ["IV", 4];
-    // 3 ~ 1
-  } else if (value < 4 && value > 0) {
-    return ["I", 1];
-  } else {
-    return ["hoge", 1000000];
-  }
-};
+const map: [string, number][] = [
+  ["M", 1000],
+  ["CM", 900],
+  ["D", 500],
+  ["CD", 400],
+  ["C", 100],
+  ["XC", 90],
+  ["L", 50],
+  ["XL", 40],
+  ["X", 10],
+  ["IX", 9],
+  ["V", 5],
+  ["IV", 4],
+  ["I", 1],
+];
 
 function intToRoman(num: number): string {
-  let currentValue = num;
-  const romanNumerals = [];
+  let result = "";
+  let remainingNumber = num;
 
-  while (currentValue > 0) {
-    const [romanNum, value] = getRomanNum(currentValue);
-    romanNumerals.push(romanNum);
-    currentValue = currentValue - value;
+  for (const [symbol, value] of map) {
+    while (remainingNumber >= value) {
+      result += symbol;
+      remainingNumber -= value;
+    }
   }
 
-  return romanNumerals.join("");
-    
+  return result;
 };
