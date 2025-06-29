ff8d64aa710a7a75fcc8f22678f12a75786dae7e
hiro266 <52743869+hiro266@users.noreply.github.com>
2025-06-29 21:02:31 +0900

    Time: 6 ms (61.54%) | Memory: 57.9 MB (83.85%) - LeetSync

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.ts
@@ -0,0 +1,14 @@
+function smallerNumbersThanCurrent(nums: number[]): number[] {
+  const result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let count = 0;
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] > nums[j]) count++;
+    }
+    result.push(count);
+  }
+
+  return result;
+
+};
